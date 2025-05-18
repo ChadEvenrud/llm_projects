8957e6e9deb5b06a137189c709bda5c9dd20b1e7
ChadEvenrud <chadevenrud@hotmail.com>
2025-05-18 07:47:17 -0700

    added new website class

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ openai = OpenAI()
 # response = openai.chat.completions.create(model="gpt-4o-mini", messages=[{"role":"user", "content":message}])
 # print(response.choices[0].message.content)
 
+headers = {
+ "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+}
+
 
 
 class Website:
@@ -22,8 +26,11 @@ class Website:
         """
         Create this Website object from the given url using the BeautifulSoup library
         """
-        headers = {"User-Agent""Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
+        
         self.url = url
+        headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+        }
         response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
         self.title = soup.title.string if soup.title else "No title found"
@@ -31,9 +38,22 @@ class Website:
             irrelevant.decompose()
         self.text = soup.body.get_text(separator="\n", strip=True)
         
-
+class Prompts:
+    """
+    Creates LLM System Prompts and User Prompts       
+    """
+    
+    def __init__(self):
+    
+        self.default_system = "You are an assistant that analyzes the contents of a website and provides a short summary, ignoring text that might be navigation related. Respond in markdown."   
+    
+    def user_prompt(self, message, website):
+        self.default_user_message =         
+        
 
 if __name__ == "__main__":
     new_site = Website("https://cnn.com")
-    print(new_site.text)
-    print("done")
+    
+    llm_prompts = Prompts()
+    print(llm_prompts.default_system)
+    
